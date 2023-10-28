9466dd4a94b2c5168954dd4b05e94b73dd03b57b
bannythebanhammerbunny <bannythebanhammerbunny@gmail.com>
2023-10-28 17:28:29 -0400

    a

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yuh huh</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <ul>
+            <li>home</li>
+            <li>ok</li>
+            <li>book</li>
+            <li>movie</li>
+        </ul>
+    </header>
+    <main>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
