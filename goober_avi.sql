0b777d9b4497e05680c41f0c002424c3f2ac7f08
bannythebanhammerbunny <bannythebanhammerbunny@gmail.com>
2023-10-28 17:16:13 -0400

    Create goober_avi.sql

---
+++ goober_avi.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 28, 2023 at 09:15 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `goober.avi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `author` text NOT NULL,
+  `length` int NOT NULL,
+  `rating` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `name`, `author`, `length`, `rating`) VALUES
+(1, 'diariaryie of a wimpy gregory', 'kineff jinney', 224, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE IF NOT EXISTS `movie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `author` text NOT NULL,
+  `length` int NOT NULL,
+  `funny` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `movie`
+--
+
+INSERT INTO `movie` (`id`, `name`, `author`, `length`, `funny`) VALUES
+(1, 'morbius', 'matt sazama', 6240, 6),
+(2, 'angry birb', 'jon vitti', 5820, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ok`
+--
+
+DROP TABLE IF EXISTS `ok`;
+CREATE TABLE IF NOT EXISTS `ok` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `album` text NOT NULL,
+  `comment` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `ok`
+--
+
+INSERT INTO `ok` (`id`, `name`, `author`, `album`, `comment`) VALUES
+(1, 'an eternity more scarlet than red', 'zun', 'no', 'loops very well. in the context of the origin, you wont be hearing it for long'),
+(9, 'Locked Girl ~ The girls Sealed Room', 'drunk guy', 'retrospective 53 minutes', 'dark, mysterious. the song is short-lived though');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
