627247288c00a3c23a4a0ff8172f63190735e552
bannythebanhammerbunny <bannythebanhammerbunny@gmail.com>
2023-10-29 20:58:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,22 @@
 </head>
 <body>
     <header>
-        <ul>
-            <li>home</li>
-            <li>ok</li>
-            <li>book</li>
-            <li>movie</li>
-        </ul>
+        <br style="margin-bottom: 10px;">
+        <a href="index.php" style="float: left;">
+            <img class="header_logo" src="../images/logo-cutout.png" alt="Logo">
+            <h1 class="page_title">noob pro hacker 2010 video compilamtion</h1>
+        </a>
+        <a href="" style="float: left;">
+            <p style="color: #FFFFFF; float: left; margin-left: 5vw; font-size: 35px;">HOURS</p>
+        </a>
+        <a href="" style="float: left;">
+            <p style="color: #FFFFFF; float: left; margin-left: 5vw; font-size: 35px;">PRODUCTS</p>
+        </a>
+        <a href="" style="float: left;">
+            <p style="color: #FFFFFF; float: left; margin-left: 5vw; font-size: 35px;">LOGOUT</p>
+        </a>
+        <br style="margin-bottom: 10px; clear: both;">
+        <br style="margin-bottom: 10px; clear: both;">
     </header>
     <main>
     </main>
