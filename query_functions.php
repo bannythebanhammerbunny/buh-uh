5fda42578ac9112d64ba877db387d8df6cfd387e
bannythebanhammerbunny <bannythebanhammerbunny@gmail.com>
2023-11-03 22:13:38 -0400

    imported files form the shop template

---
+++ query_functions.php
@@ -0,0 +1,141 @@
+<?php
+
+// 
+// Admins
+// 
+// Find an admin by id
+function get_admin_by_id($id)
+{
+	global $db;
+
+	$sql = "SELECT * FROM `admin` ";
+	$sql .= "WHERE id='" . db_escape($db, $id) . "' ";
+	$sql .= "LIMIT 1";
+	$result = mysqli_query($db, $sql);
+	confirm_result_set($result);
+	$admin = mysqli_fetch_assoc($result); // get first
+	mysqli_free_result($result);
+	return $admin; // returns an assoc. array
+}
+
+
+// Find an admin by username, used in admin login
+function get_admin_by_username($username)
+{
+	global $db;
+
+	$sql = "SELECT * FROM `admin` ";
+	$sql .= "WHERE username='" . db_escape($db, $username) . "' ";
+	$sql .= "LIMIT 1";
+	$result = mysqli_query($db, $sql);
+	confirm_result_set($result);
+	$admin = mysqli_fetch_assoc($result); // find first
+	mysqli_free_result($result);
+	return $admin; // returns an assoc. array
+}
+
+// 
+// Products
+// 
+function get_all_products()
+{
+	global $db;
+
+	$sql = "SELECT * FROM `products` ";
+	$sql .= "ORDER BY brand ASC";
+	$result = mysqli_query($db, $sql);
+	confirm_result_set($result);
+	return $result; // returns an assoc. array
+}
+
+function get_product_by_id($id)
+{
+	global $db;
+	$sql = "SELECT * FROM `products` WHERE `id` = $id";
+	$result = mysqli_query($db, $sql);
+	confirm_result_set($result);
+	$p = mysqli_fetch_assoc($result);
+	mysqli_free_result($result);
+	return $p; // returns an assoc. array
+}
+
+function update_product_by_id($id, $brand, $name, $img)
+{
+	global $db;
+
+	$sql = "UPDATE `products` SET `brand`='$brand',`name`='$name',`img`='$img' WHERE `id`='$id';";
+
+	$result = mysqli_query($db, $sql);
+	// For UPDATE statements, $result is true/false
+	if ($result) {
+		return true;
+	} else {
+		// UPDATE failed
+		echo mysqli_error($db);
+		db_disconnect($db);
+		exit;
+	}
+}
+
+function delete_product_by_id($id)
+{
+	global $db;
+
+	$sql = "DELETE FROM `products` WHERE `id` = $id;";
+	$result = mysqli_query($db, $sql);
+
+	// For DELETE statements, $result is true/false
+	if ($result) {
+		return true;
+	} else {
+		// DELETE failed
+		echo mysqli_error($db);
+		db_disconnect($db);
+		exit;
+	}
+}
+
+function insert_product($brand, $name, $img) {
+	global $db;
+
+	$sql = "INSERT INTO `products`(`brand`, `name`, `img`) VALUES ('$brand','$name','$img')";
+
+	$result = mysqli_query($db, $sql);
+	if ($result) {
+		return true;
+	} else {
+		// UPDATE failed
+		echo mysqli_error($db);
+		db_disconnect($db);
+		exit;
+	}
+}
+
+// Hours
+function get_all_hours()
+{
+	global $db;
+
+	$sql = "SELECT * FROM `hours` ";
+	$result = mysqli_query($db, $sql);
+	confirm_result_set($result);
+	return $result; // returns an assoc. array
+}
+
+function update_hours_by_id($mon, $tue, $wed, $thu, $fri, $sat, $sun)
+{
+	global $db;
+
+	$sql = "UPDATE `hours` SET `mon`='$mon',`tue`='$tue',`wed`='$wed',`thu`='$thu',`fri`='$fri',`sat`='$sat',`sun`='$sun' WHERE `id`=1;";
+
+	$result = mysqli_query($db, $sql);
+	// For UPDATE statements, $result is true/false
+	if ($result) {
+		return true;
+	} else {
+		// UPDATE failed
+		echo mysqli_error($db);
+		db_disconnect($db);
+		exit;
+	}
+}
