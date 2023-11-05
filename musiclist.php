7b83032ee473769d52f913f12f1da13670974218
bannythebanhammerbunny <bannythebanhammerbunny@gmail.com>
2023-11-04 22:37:50 -0400

    yeh

--- musiclist.php
+++ musiclist.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,6 +29,24 @@
         <br style="margin-bottom: 10px; clear: both;">
     </header>
     <main>
+        
+    <table style="border-collapse: collapse; width: 100%;">
+            <tr>
+                <th style="border: 1px solid #202020; text-align: center; padding: 8px;">id</th>
+                <th style="border: 1px solid #202020; text-align: center; padding: 8px;">Name</th>
+                <th style="border: 1px solid #202020; text-align: center; padding: 8px;">Author</th>
+                <th style="border: 1px solid #202020; text-align: center; padding: 8px;">length</th>
+                <th style="border: 1px solid #202020; text-align: center; padding: 8px;">comment</th>
+            </tr>
+            <?php while ($movie = mysqli_fetch_assoc($movies)) { ?>
+                <tr>
+                    <td style="border: 1px solid #202020; text-align: center; padding: 8px;"><?php echo $movie['id']; ?></td>
+                    <td style="border: 1px solid #202020; text-align: center; padding: 8px;"><?php echo $movie['name']; ?></td>
+                    <td style="border: 1px solid #202020; text-align: center; padding: 8px;"><?php echo $movie['author']; ?></td>
+                    <td style="border: 1px solid #202020; text-align: center; padding: 8px;"><?php echo $movie['length']; ?></td>
+                    <td style="border: 1px solid #202020; text-align: center; padding: 8px;"><?php echo $movie['comment']; ?></td>
+            <?php } ?>
+        </table>
     </main>
     <footer>
 
